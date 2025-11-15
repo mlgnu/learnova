f95fe659732ed085c1806f7eca77974e1585be60
mlgnu <mohamedtahahelmy@gmail.com>
2025-11-15 21:33:47 +0800

    chore: project initial setup

---
+++ src/test/java/org/mlgnu/learnova/LearnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mlgnu.learnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
